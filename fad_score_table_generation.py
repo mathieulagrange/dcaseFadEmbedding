cfc228774532f10c2cc6ab8ddcb9d6309b01339d
modantailleur <modan.tailleur@gmail.com>
2023-12-07 17:48:02 +0100

    remove the argument for embedding in fad score generation

--- fad_score_table_generation.py
+++ fad_score_table_generation.py
@@ -9,9 +9,10 @@ import argparse
 
 def main(config):
   reference = 'eval'
-  embedding_list = config.emb
 
   excel_file = './excel_files/fadScores.xlsx'
+
+  embedding_list = main_doce.experiment.fad.embedding
 
   for embedding in embedding_list:
     (data_pred, settings_pred_total, header_pred_total) = main_doce.experiment.get_output(
@@ -19,7 +20,8 @@ def main(config):
       selector = {"reference":reference, "embedding":embedding},
       path = "fad",
       )
-    
+    if len(data_pred) == 0:
+      continue
     with pd.ExcelWriter(excel_file, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
       score_list = []
       for idx, (pred, settings_pred) in enumerate(zip(data_pred, settings_pred_total)):
@@ -36,11 +38,9 @@ def main(config):
       df = df.pivot(index='alg_code', columns='category', values='fad')
 
       df['avg_category_FAD'] = df.mean(axis=1)
-      df.to_excel(writer, index=True, sheet_name=embedding)
+      df.to_excel(writer, index=True, sheet_name=dict_system_info['embedding'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Modifies fadScores.xlsx to add the FAD for the new specified embeddings')
-    parser.add_argument('--emb', type=list, default=['vggish', 'clap-2023', 'clap-laion-audio', 'MERT-v1-95M-1', 'encodec-emb', 'encodec-emb-48k'],
-                        help="The list of embeddings to calculate - default: ['vggish', 'clap-2023', 'clap-laion-audio', 'MERT-v1-95M-1', 'encodec-emb', 'encodec-emb-48k']")
     config = parser.parse_args()
     main(config)
